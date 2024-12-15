821c3603f1e33af410f4332ca6fbaf79ed369102
Ahmed Salah Elsharkawy <96499153+Ah-sala7@users.noreply.github.com>
2024-12-16 01:00:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,6 +82,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+
 
